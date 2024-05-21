cc6d95973a40fc8e728725c371c6abd19de100f0
tanureet kaur <tanureetkaur@academin.rrc.ca>
2024-05-21 09:01:25 -0500

    correct the code with some changes

--- ChallengeU04.rb
+++ ChallengeU04.rb
@@ -6,13 +6,15 @@ uri = URI(url)
 response = Net::HTTP.get(uri)
 tree_data = JSON.parse(response)
 
-ash_trees = 0
-
-tree_data.each do |tree|
-  species_name = tree['species_name']
-  if species_name && species_name.downcase.include?('ash')
-    ash_trees += 1
+def count_ash_trees(tree_data)
+  ash_tree_count = 0
+  tree_data.each do |tree|
+    if tree['common_name']&.downcase&.include?('ash') || tree['scientific_name']&.downcase&.include?('fraxinus')
+      ash_tree_count += 1
+    end
   end
+  ash_tree_count
 end
 
-puts "Number of Ash trees in the dataset: #{ash_trees}"
+ash_count = count_ash_trees(tree_data)
+puts "Number of ash trees in Winnipeg: #{ash_count}"
